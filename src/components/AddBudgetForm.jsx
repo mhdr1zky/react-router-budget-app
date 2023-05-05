240bea00f40428bb32df4b8f5cbd3004c008722a
mhdr1zky <89838101+mhdr1zky@users.noreply.github.com>
2023-05-05 19:04:20 +0800

    To Rp

--- src/components/AddBudgetForm.jsx
+++ src/components/AddBudgetForm.jsx
@@ -49,7 +49,7 @@ const AddBudgetForm = () => {
             step="0.01"
             name="newBudgetAmount"
             id="newBudgetAmount"
-            placeholder="e.g., $350"
+            placeholder="e.g., Rp.100000"
             required
             inputMode="decimal"
           />
